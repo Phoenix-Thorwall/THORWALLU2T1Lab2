3b02afd5fb2d75de5374caca8b394eb72b5a2690
student <sample@bths.edu>
2022-09-28 12:24:01 -0400

    What I did in class

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class RectangleRunner {
+import java.util.Scanner;
+public class RectangleRunner
+{
+    public static void main(String[] args)
+    {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter rectangle 1 length: ");
+        int rect1Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 width: ");
+        int rect1Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 height: ");
+        double rect1Height = myScanner.nextDouble();
+        System.out.print("Enter rectangle 2 length: ");
+        int rect2Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 width: ");
+        int rect2Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 height: ");
+        double rect2Height = myScanner.nextDouble();
+
+        // write the rest of your program below
+
+
+    }
 }
+
